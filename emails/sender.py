8e041a209eff93b2a1fd7113a1bf835f023d5fd5
Henry Liu <thenrikie@hotmail.com>
2015-05-05 22:52:59 +0800

    more emails template...

--- emails/sender.py
+++ emails/sender.py
@@ -20,5 +20,32 @@ def signup_customer(email, content):
 def signup_company(email, content):
 	send(email, content, 'Welcome to RatedFrog', 'signup_company')
 
+def new_proposal(email, content):
+	subject = 'New proposal from ' + content['company_contact'] + ' at ' + content['company_name']
+	send(email, content, subject, 'new_proposal')
+
+def project_pitched(email, content):
+	subject = 'Thanks for submitting your proposal'
+	send(email, content, subject, 'project_pitched')
+
+def project_cancel(email, content):
+	subject = content['customer_name'] +  ' has cancelled their project'
+	send(email, content, subject, 'project_cancel')
+
+def hired(email, content):
+	subject = 'Congrats, you have been hired by ' + content['customer_name']
+	send(email, content, subject, 'hired')
+
+def rejected(email, content):
+	subject = content['customer_name'] + ' decided to hire someone else'
+	send(email, content, subject, 'rejected')
+
+def message_for_customer(email, content):
+	subject = 'You have a new message from ' + content['company_contact']
+	send(email, content, subject, 'message_for_customer')
+
+def message_for_company(email, content):
+	subject = 'You have a new message from ' + content['customer_name']
+	send(email, content, subject, 'message_for_company')
 
 
